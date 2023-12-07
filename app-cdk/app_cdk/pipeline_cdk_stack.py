3ae0b3960e671334f0bcd43bf7f6ee90847644d2
eseantatum <eseantatum@gmail.com>
2023-12-07 11:03:35 -0500

    Implement CI Pipeline

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -3,6 +3,9 @@ from aws_cdk import (
     Stack,
     CfnOutput,
     aws_codecommit as codecommit,
+    aws_codepipeline as codepipeline,
+    aws_codebuild as codebuild,
+    aws_codepipeline_actions as codepipeline_actions,
 )
 
 class PipelineCdkStack(Stack):
@@ -15,6 +18,51 @@ class PipelineCdkStack(Stack):
             self, 'CICD_Workshop',
             repository_name = 'CICD_Workshop',
             description = 'Repository for my application code and infrastructure'
+        )
+
+        pipeline = codepipeline.Pipeline(
+            self, 'CICD_Pipeline',
+            cross_account_keys = False
+        )
+
+        code_quality_build = codebuild.PipelineProject(
+            self, 'Code Quality',
+            build_spec = codebuild.BuildSpec.from_source_filename('./buildspec_test.yml'),
+            #build_spec = codebuild.BuildSpec.from_object({
+            #    'version': '0.2'
+            #}),
+            environment = codebuild.BuildEnvironment(
+                build_image = codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged = True,
+                compute_type = codebuild.ComputeType.LARGE,
+            ),
+        )
+
+        source_output = codepipeline.Artifact()
+        unit_test_output = codepipeline.Artifact()
+
+        source_action = codepipeline_actions.CodeCommitSourceAction(
+            action_name = 'CodeCommit',
+            repository = repo,
+            output = source_output,
+            branch = 'main'
+        )
+
+        pipeline.add_stage(
+            stage_name = 'Source',
+            actions = [source_action]
+        )
+
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name = 'Unit-Test',
+            project = code_quality_build,
+            input = source_output,  # The build action must use the CodeCommitSourceAction output as input.
+            outputs = [unit_test_output]
+        )
+
+        pipeline.add_stage(
+            stage_name = 'Code-Quality-Testing',
+            actions = [build_action]
         )
 
         CfnOutput(
